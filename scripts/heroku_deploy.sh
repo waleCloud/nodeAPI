2270b099bb30dd3e4aa1a8bf32dcca8dc3e75704
waleCloud <walexrvp@gmail.com>
2019-09-25 21:28:49 +0100

    fix failing build

--- scripts/heroku_deploy.sh
+++ scripts/heroku_deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-docker login -u _ --password=$HEROKU_API_KRY --password-stdin registry.heroku.com
+heroku plugins:install @heroku-cli/plugin-container-registry
+docker login -u _ --password=$HEROKU_API_KRY registry.heroku.com
 heroku container:push web --app $HEROKU_APP_NAME
 echo "done"
