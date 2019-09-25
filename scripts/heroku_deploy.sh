ce15ec3da440955346b4b3cadb5287383e891f45
waleCloud <walexrvp@gmail.com>
2019-09-25 20:24:51 +0100

    Retry auto deploy travis-heroku

--- scripts/heroku_deploy.sh
+++ scripts/heroku_deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-docker login -e -u --password=$HEROKU_API_KEY registry.heroku.com
+heroku plugins:install @heroku-cli/plugin-container-registry
+docker login -u _ --password=$HEROKU_API_KEY registry.heroku.com
 heroku container:push web --app $HEROKU_APP_NAME
+echo "done"
