80dfef4ea872a1559c08f40643df3965ed11219c
waleCloud <walexrvp@gmail.com>
2019-09-25 20:51:10 +0100

    Retry auto deploy travis-heroku

--- scripts/heroku_deploy.sh
+++ scripts/heroku_deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-docker login -u _ --password=$HEROKU_API_KEY registry.heroku.com
+echo "$DOCKER_PASSWORD" | docker login -u _ --password=$HEROKU_API_KEY --password-stdin registry.heroku.com
 heroku container:push web --app $HEROKU_APP_NAME
 echo "done"
