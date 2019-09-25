44bbf00678fcfc91dd0a2af7e88686d59aa35ec8
waleCloud <walexrvp@gmail.com>
2019-09-25 21:10:59 +0100

    retry failed build n deploy

--- scripts/heroku_deploy.sh
+++ scripts/heroku_deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-echo "$DOCKER_PASSWORD" | docker login -u _ --password=$HEROKU_API_KEY --password-stdin registry.heroku.com
+echo "$DOCKER_PASSWORD" | docker login -e _ -u _ --password=$HEROKU_API_KEY registry.heroku.com
 heroku container:push web --app $HEROKU_APP_NAME
 echo "done"
