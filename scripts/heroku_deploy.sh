b441268a49455d6980f1647c132a56c2b10d807c
waleCloud <walexrvp@gmail.com>
2019-09-25 21:35:37 +0100

    fix failing build

--- scripts/heroku_deploy.sh
+++ scripts/heroku_deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 heroku plugins:install @heroku-cli/plugin-container-registry
-docker login -u _ --password=$HEROKU_API_KRY registry.heroku.com
+docker login -u _ --password=$(heroku auth:token) registry.heroku.com
 heroku container:push web --app $HEROKU_APP_NAME
 echo "done"
