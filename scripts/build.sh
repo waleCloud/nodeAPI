131b2de8a980c0afcf12c5777879ab887084ac5a
waleCloud <walexrvp@gmail.com>
2019-09-26 20:11:18 +0100

    First serious attempt to auto deploy again

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 if [ "$TRAVIS_BRANCH" = "master" ]; then
-    TAG="latest"
+  TAG="latest"
 else
-    TAG="$TRAVIS_BRANCH"
+  TAG="$TRAVIS_BRANCH"
 fi
 USER="walecloud/testapi"
 docker build -f Dockerfile -t $USER:$TAG .
-docker push $USER:$TAG
+docker tag $USER:$TAG registry.heroku.com/$HEROKU_APP_NAME/web
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
   wget -qO- https://toolbelt.heroku.com/install.sh | sh
   heroku plugins:install @heroku-cli/plugin-container-registry
-  # login to registry heroku
-  echo "$HEROKU_PASSWORD" | docker login -u "$HEROKU_USERNAME" --password-stdin registry.heroku.com
+  # deploy to docker & heroku registy
+  if [ "$TRAVIS_BRANCH" = "master" ]; then
+    TAG="latest"
+  else
+    TAG="$TRAVIS_BRANCH"
+  fi
+  USER="walecloud/testapi"
+  docker push $USER:$TAG
   docker push registry.heroku.com/$HEROKU_APP_NAME/web;
+  # Deploy docker container as a web app and release it
   heroku container:push web --app $HEROKU_APP_NAME
   heroku container:release web --app $HEROKU_APP_NAME
   echo "done"
