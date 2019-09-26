69ddf9a5540305d31885e8ed6265dbddf5d48185
waleCloud <walexrvp@gmail.com>
2019-09-26 14:21:58 +0100

    change tag slug

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -5,5 +5,6 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
-docker push $TRAVIS_REPO_SLUG:$TAG
+USER="walecloud"
+docker build -f Dockerfile -t $USER:$TAG .
+docker push $USER:$TAG
